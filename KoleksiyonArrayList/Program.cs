aaf15f1990776527c0d9fb93c7ed7fe3026cb032
smtdemirel <sametdemirel74@gmail.com>
2022-02-05 23:59:56 +0300

    Koleksiyon Array List Ödev

---
+++ KoleksiyonArrayList/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoleksiyonArrayList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList liste = new ArrayList();
+            liste.Add("Samet");
+            liste.Add(26);
+
+            //Liste içerisindeki elemanlara erişim
+            Console.WriteLine(liste[1]);
+
+            foreach (var i in liste)
+            {
+                Console.WriteLine(i);
+            }
+
+            //AddRange
+            string[] renkler = { "kırmızı", "sarı", "yeşil" };
+            List<int> sayilar = new List<int> { 3,45,23,132,534};
+            liste.AddRange(renkler);
+            liste.AddRange(sayilar);
+
+            foreach(var i in liste)
+            {
+                Console.WriteLine(i);
+            }
+            liste.Clear();
+            liste.AddRange(sayilar);
+            //Sort
+            liste.Sort();
+
+            foreach(var i in liste)
+            {
+                Console.WriteLine(i);
+            }
+
+            //Binary Search
+            Console.WriteLine(liste.BinarySearch(14));
+            
+            //Reverse
+            liste.Reverse();
+            foreach(var i in liste)
+            {
+                Console.WriteLine(i);
+            }
+
+            //Clear
+            liste.Clear();
+        }
+    }
+}
