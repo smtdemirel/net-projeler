3f986482eb5c958409321b66b4e85bbf029d49fa
smtdemirel <sametdemirel74@gmail.com>
2022-02-06 23:20:03 +0300

    Static Sınıf Ödev

---
+++ StaticSinif/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticSinif
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Çalışan Sayısı: {0}",Calisan.CalisanSayisi);
+
+            Calisan calisan1 = new Calisan("Samet","Demirel","Yazılım");
+            Console.WriteLine("Çalışan Sayısı: {0}", Calisan.CalisanSayisi);
+            Calisan calisan2 = new Calisan("Samet", "Demirel", "Yazılım");
+            Calisan calisan3 = new Calisan("Samet", "Demirel", "Yazılım");
+            Calisan calisan4 = new Calisan("Samet", "Demirel", "Yazılım");
+            Console.WriteLine("Çalışan Sayısı: {0}", Calisan.CalisanSayisi);
+
+            Console.WriteLine("Toplama İşlemi Sonucu: {0}",Islemler.Topla(2,5));
+            Console.WriteLine("Çıkarma İşlemi Sonucu: {0}",Islemler.Cikar(12,5));
+        }
+    }
+    class Calisan
+    {
+        private static int calisanSayisi;
+
+        public static int CalisanSayisi { get => calisanSayisi;}
+
+        private string Isim;
+        private string Soyisim;
+        private string Departman;
+
+        static Calisan()
+        {
+            calisanSayisi=0;
+        }
+
+        public Calisan(string ısim, string soyisim, string departman)
+        {
+            Isim = ısim;
+            Soyisim = soyisim;
+            Departman = departman;
+            calisanSayisi++;
+        }
+
+        
+    }
+    static class Islemler
+    {
+        public static int Topla(int sayi1, int sayi2) 
+        {
+            return sayi1 + sayi2;
+        }
+        public static int Cikar(int sayi1, int sayi2)
+        {
+            return sayi1 - sayi2;
+        }
+    }
+}
